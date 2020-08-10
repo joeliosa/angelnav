995a81f9e580ae7980d00b61b989cb7f43207df1
Joel Saarinen <johowin@gmail.com>
2020-08-10 10:34:22 +0300

    Second commit

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ new Vue({
     el: '#app-vue',
     data() {
       return {
-        users: [],
+        coordinates: [],
+        names: []
       }
     },
     methods: {
@@ -10,7 +11,17 @@ new Vue({
         axios.get('https://discover.search.hereapi.com/v1/discover?at=52.5228,13.412&q=petrol+station&limit=5&lang=en-US&apiKey=ZVa8tOAMzIh_Wm5u8GGARwHxV12KfLFPUbGMNVul9xo')
           .then((response) => {
             const data = response.data; // [{}, {}]
-            this.users = data;
+            const transformeddata = data['items']
+            for (var i = 0; i < transformeddata.length; i++) { 
+              var entry = transformeddata[i]
+              this.coordinates.push(entry['position'])
+            }
+            for (var i = 0; i < transformeddata.length; i++) { 
+              var entry = transformeddata[i]
+              this.names.push(entry['title'])
+            }
+            //for (var k = 0; i < transformeddata.length)
+            //this.users = transformeddata;
           });
       },
     }
